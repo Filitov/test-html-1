7cf576b4ddf8aff585dbc0024d9075de36328d25
filitov <filitov@gmail.com>
2020-03-25 02:24:32 +0800

    test1

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="zh_Hant">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>範例1-2</title>
 </head>
 <body>
